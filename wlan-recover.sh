558a745609d3dcc6e6b8359989317a52164d0979
Olav68 <olav@andersskog.no>
2025-05-21 11:25:40 +0200

    V2

--- wlan-recover.sh
+++ wlan-recover.sh
@@ -1,11 +1,23 @@
-# Sjekk status på wlan0
-# Hvis wlan0 er nede, sett den opp
-# Hvis wlan0 er oppe, gjør ingenting
-IFSTATE=$(cat /sys/class/net/wlan0/operstate)
+#!/bin/bash
+
+TAG="RFIDMusicBox"
+
+# Finn status på wlan0
+IFSTATE=$(cat /sys/class/net/wlan0/operstate 2>/dev/null)
+
+if [ -z "$IFSTATE" ]; then
+    logger -t "$TAG" "Fant ikke grensesnitt wlan0 – finnes det?"
+    exit 1
+fi
+
+logger -t "$TAG" "Status for wlan0: $IFSTATE"
 
 if [ "$IFSTATE" = "down" ]; then
-    logger "RFIDMusicBox: wlan0 var nede – setter opp"
+    logger -t "$TAG" "wlan0 er nede – forsøker å sette opp"
     ip link set wlan0 up
+    sleep 2
+    NEWSTATE=$(cat /sys/class/net/wlan0/operstate)
+    logger -t "$TAG" "Ny status etter 'ip link set': $NEWSTATE"
 else
-    logger "RFIDMusicBox: wlan0 er allerede oppe – ingen handling"
+    logger -t "$TAG" "wlan0 er oppe – ingen tiltak"
 fi
