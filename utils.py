30184da7bcfacc35a438aee0c666edbffc954477
Olav68 <olav@andersskog.no>
2025-05-22 14:41:47 +0200

    List debug

--- utils.py
+++ utils.py
@@ -72,7 +72,8 @@ def play_song(filepath):
         append_log(f"❌ Feil ved avspilling i play_song(): {e}")
 
 def is_youtube_playlist(url):
-    return "list=" in url
+    # Hvis den inneholder en `list=`-parameter, er det en spilleliste
+    return "list=" in url and "youtube.com" in url
 
 def download_youtube_playlist(url, target_folder):
     try:
